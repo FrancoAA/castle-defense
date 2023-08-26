fe11c9a9b4b03a4cc87b6a53a6af802d742cd009
Franco Alarcon <franco.alarcon@attackiq.com>
2023-08-26 19:39:14 -0300

    feat: :sparkles: adds enemy shooting mechanics

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import kaboom, { GameObj } from "kaboom";
+import kaboom, { GameObj, Vec2 } from "kaboom";
 
 const ENEMY_SPEED = 100;
 const ENEMY_ATTACK_DMG = 1;
@@ -10,7 +10,9 @@ const ENEMY_TYPES = {
   },
   "dino": {
     speed: ENEMY_SPEED * 0.75,
-    attack: 1,
+    attackSpeed: 1,
+    projectileSpeed: 150,
+    attack: 0.5,
     health: 1,
   },
   "gigagantrum": {
@@ -41,6 +43,25 @@ k.loadSprite("gigagantrum", "gigagantrum.png");
 k.loadSprite("sword", "sword.png");
 k.loadSprite("gun", "gun.png");
 
+function getDirection(vector1, vector2) {
+  const deltaX = vector2.x - vector1.x;
+  const deltaY = vector2.y - vector1.y;
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 0) {
+          return k.RIGHT;
+      } else {
+          return k.LEFT;
+      }
+  } else {
+      if (deltaY > 0) {
+          return k.DOWN;
+      } else {
+          return k.UP;
+      }
+  }
+}
+
 function grow(rate) {
   return {
     update() {
@@ -57,7 +78,6 @@ function healthBar(barHeight = 10) {
     require: ["health"],
     add() {
       this.on("hurt", (amount: number) => {
-        console.log("hurt", amount);
         this.add([
           k.text(`${amount}`, { size: 20 }),
           k.color(k.RED),
@@ -103,6 +123,25 @@ function randomPos() {
   }
 }
 
+function spawnBullet(startPos: Vec2, targetPos: Vec2, speed: number, attack: number = 1) {
+  const _startPos = startPos.sub(0, -30);
+
+  return k.add([
+    k.circle(1),
+    k.color(k.BLUE),
+    k.area(),
+    k.pos(_startPos),
+    k.move(targetPos.sub(_startPos).unit(), speed),
+    k.offscreen({ destroy: true }),
+    'bullet',
+    'enemy',
+    {
+      attack,
+      prevPos: _startPos,
+    }
+  ]);
+}
+
 function spawnEnemy() {
   const type = k.choose(Object.keys(ENEMY_TYPES));
   const baseStats = ENEMY_TYPES[type];
@@ -110,33 +149,64 @@ function spawnEnemy() {
   const speed = k.rand(baseStats.speed * (1 / level), baseStats.speed);
   const attack = (baseStats.attack + (level * 0.1));
   const pos = randomPos();
+  const faceDir = getDirection(player.pos, pos);
+  console.log('getDirection', faceDir);
+  const sprite = k.sprite(type, { flipX: faceDir === k.LEFT });
 
   const enemy = k.add([
-    k.sprite(type),
+    sprite,
     k.health(level),
     k.pos(pos),
     k.area(),
+    k.timer(),
     k.move(player.pos.sub(pos).unit(), speed),
     healthBar(),
     "enemy",
     {
+      ...baseStats,
       level,
       attack,
       speed,
     },
   ]);
 
-  return enemy;
+  if (type === 'dino') {
+    enemy.wait(enemy.attackSpeed, () => {
+      spawnBullet(enemy.pos, player.pos, enemy.projectileSpeed);
+    });
+  }
+
+  // spawn another enemy every 3 seconds
+  k.wait(1, spawnEnemy);
 }
 
-function addExplode(p, n, rad, size) {
+function addExplode(p, n, rad, size, color = k.RED) {
   for (let i = 0; i < n; i++) {
     k.wait(k.rand(n * 0.1), () => {
       for (let i = 0; i < 2; i++) {
         k.add([
           k.pos(p.add(k.rand(k.vec2(-rad), k.vec2(rad)))),
           k.rect(4, 4),
-          k.color(k.RED),
+          k.color(color),
+          k.scale(1 * size, 1 * size),
+          k.lifespan(0.1),
+          grow(k.rand(48, 72) * size),
+          k.anchor("center"),
+        ]);
+      }
+    });
+  }
+}
+
+function addTrail(startPos: Vec2, numPart: number, rad: number, size: number) {  
+  for (let i = 0; i < numPart; i++) {
+    k.wait(k.rand(numPart * 0.1), () => {
+      for (let i = 0; i < 2; i++) {
+        k.add([
+          k.pos(startPos.add(k.rand(k.vec2(-rad), k.vec2(rad)))),
+          k.rect(4, 4),
+          k.color(k.WHITE),
+          k.opacity(0.3),
           k.scale(1 * size, 1 * size),
           k.lifespan(0.1),
           grow(k.rand(48, 72) * size),
@@ -172,6 +242,7 @@ const swordCursor = k.add([
   k.z(100),
   k.scale(0.5),
   k.rotate(-45),
+  k.opacity(1),
   'cursor',
   'sword',
 ]);
@@ -182,9 +253,12 @@ const gunCursor = k.add([
   k.sprite("gun"),
   k.z(100),
   k.scale(0.5),
+  k.opacity(1),
   'cursor',
   'gun',
 ]);
+
+spawnEnemy();
 
 k.onUpdate(() => {
   infoLabel.text = `
@@ -205,15 +279,20 @@ k.onUpdate(() => {
   }
 });
 
+k.onUpdate('bullet', (p) => {
+  addTrail(p.prevPos, 3, 2, 0.2);
+  p.prevPos = p.pos;
+});
+
 k.onCollide("enemy", "player", (e) => {
   infoLabel.combo = 0;
-  player.hurt(1);
+  player.hurt(e.attack);
   k.shake(3);
   k.destroy(e);
 });
 
-k.onClick((btn) => {
-  if (btn !== 'right') return;
+k.onClick((mouseBtn) => {
+  if (mouseBtn !== 'right') return;
   currentWeapon = currentWeapon === 'sword' ? 'gun' : 'sword';
 });
 
@@ -245,8 +324,6 @@ k.on("death", "enemy", (e) => {
 k.on("death", "player", (p) => {
   k.go("gameover");
 });
-
-k.loop(1, spawnEnemy);
 
 k.scene("gameover", () => {
   k.add([
